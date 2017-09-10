e42b33eb37c08cf86dbc9306a37dabffc0cf0ae5
Drewmalino <wojtczukmariusz@wp.pl>
2017-09-11 00:17:41 +0200

    2nd commit (#3)
    * my first commit
    
    * my 2nd commit
    
    * Favorite Ads service implemented

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -15,6 +15,15 @@ class AccountController
             $userProduct[$i] = Product::getProductListByTableNameAndAdsId($item["subcategory_name"], $item["adsId"], $item["category_name"]);
             $i++;
         }
+
+        $getFavAdsByUserCreateId = array();
+        $getFavAdsByUserCreateId = Product::getFavAdsByCreatedUserId($userId);
+        $r = 0;
+        foreach ($getFavAdsByUserCreateId as $favItem) {
+            $userFavAd[$r] = Product::getFavAdsListByTableNameAndAdsId($favItem["subcategory_name"], $favItem["ads_id"], $favItem["category_name"]);
+            $r++;
+        }
+
         require_once(ROOT . '/views/account/index.php');
         return true;
     }
