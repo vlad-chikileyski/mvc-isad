ddb52a5b2e816f55c488fd4dae924f75dfdf0ca6
boych <boychin22@gmail.com>
2017-11-10 18:16:03 +0200

    https://adtoday.myjetbrains.com/youtrack/issue/ISAD-40

--- m/controllers/PostController.php
+++ m/controllers/PostController.php
@@ -2,13 +2,22 @@
 
 class PostController
 {
-    public function actionIndex(){
+    public function actionIndex()
+    {
 
     }
+
     public function actioncategoryList()
     {
-            require_once(ROOT . '/views/post/firststep.php');
+        require_once(ROOT . '/views/post/firststep.php');
         return true;
 
     }
+
+    public function actionCharge()
+    {
+        require_once(ROOT . '/views/payment/payment.php');
+        return true;
+    }
+
 }
