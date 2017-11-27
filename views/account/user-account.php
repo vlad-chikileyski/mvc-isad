b8b8753c46db8489ed9e607594e7b045d218b8bc
boych <boychin22@gmail.com>
2017-11-27 16:35:57 +0200

    https://adtoday.myjetbrains.com/youtrack/issue/LIQED-18

--- views/account/user-account.php
+++ views/account/user-account.php
@@ -1,4 +1,85 @@
 <?php include ROOT . '/views/layout/header.php'; ?>
+<div class="sliding-nav">
+    <a class="close-sliding-nav" href="#">Close nav</a>
+    <nav class="slide-nav-list">
+        <ul>
+            <li class="parent">
+                <a href="javascript:return;">Demos</a>
+                <ul>
+                    <li><a href="home2.html">Demo1 - Focus</a></li>
+                    <li><a href="home3.html">Demo2 - Expressive</a></li>
+                    <li><a href="home4.html">Demo3 - Minimal</a></li>
+                    <li><a href="home5.html">Demo4 - Fancy</a></li>
+                </ul>
+            </li>
+            <li class="parent">
+                <a href="#">Classified</a>
+                <ul>
+                    <li><a href="home-default.html">Home</a></li>
+                    <li><a href="listing.html">Ads Archive</a></li>
+                    <li><a href="single.html">Ad Single</a></li>
+                    <li><a href="create.html">Create Ad</a></li>
+                    <li><a href="dashboard.html">Dashboard</a></li>
+                </ul>
+            </li>
+            <li class="parent">
+                <a href="javascript:return;">Shop</a>
+                <ul>
+                    <li><a href="shop-index.html">Shop Home</a></li>
+                    <li><a href="shop-listing.html">Archive</a></li>
+                    <li><a href="shop-single.html">Single</a></li>
+                    <li><a href="cart.html">Cart</a></li>
+                    <li><a href="checkout.html">Checkout</a></li>
+                </ul>
+            </li>
+            <li class="parent">
+                <a href="javascript:return;">Pages</a>
+                <ul>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="blog-single.html">Blog Single</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                    <li><a href="terms.html">Terms</a></li>
+                </ul>
+            </li>
+            <li class="parent">
+                <a href="javascript:return;">Shortcodes</a>
+                <ul>
+                    <li><a href="elements-shortcodes.html">Elements</a></li>
+                    <li><a href="typography.html">Typography</a></li>
+                    <li><a href="elements-bootstrap.html">Bootstrap Components</a></li>
+                    <li><a href="buttons.html">Buttons</a></li>
+                </ul>
+            </li>
+
+        </ul>
+    </nav>
+</div>
+<div class="mobile-menu-wrap">
+    <a href="#" class="closeMobilMenu">close the mobile menu</a>
+    <div class="mobile-menu">
+        <div class="search-widget">
+            <input type="text" placeholder="search">
+            <button type="submit"><i class="fa fa-search"></i></button>
+        </div>
+        <ul class="menu-list">
+            <li><a href="create.html">Create an Ad</a></li>
+            <li><a href="dashboard.html">Dashboard</a></li>
+            <li><a href="listing.html">Ads Listing Page</a></li>
+            <li><a href="how-adspot-works.html">How this works</a></li>
+            <li><a href="about.html">About us</a></li>
+            <li><a href="contact.html">Contact us</a></li>
+            <li><a href="blog.html">Blog</a></li>
+            <li><a href="blog-single.html">Blog Single</a></li>
+            <li><a href="shop-index.html">Shop Page</a></li>
+            <li><a href="shop-listing.html">Shop Archive</a></li>
+            <li><a href="cart.html">Cart Page</a></li>
+            <li><a href="checkout.html">Checkout Page</a></li>
+            <li><a href="elements-shortcodes.html">Checkout Page</a></li>
+        </ul>
+    </div>
+</div>
+
     <div class="app-canvas">
         <div class="container">
             <div class="breadcrumb">
